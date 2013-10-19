36a94ab0d78c8802b2acbcbce45328af70907345
Darren Lin <dlin0915@gmail.com>
2013-10-19 15:36:15 -0700

    sorting

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019200925) do
+ActiveRecord::Schema.define(version: 20131019222340) do
 
   create_table "topics", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "votes_count"
   end
 
   create_table "votes", force: true do |t|
